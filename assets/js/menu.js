cccac27408282794d3b572449c4a1a67b791cb76
João Henrique <jh36750@gmail.com>
2024-12-23 15:40:31 -0300

    ajsute no icone de menu - mobile

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,3 +1,18 @@
+// Seleciona os botões de abrir e fechar
+const menuToggle = document.getElementById("menu-toggle");
+const closeMenu = document.getElementById("close-menu");
+const mobileMenu = document.getElementById("mobile-menu");
+
+// Evento para abrir o menu
+menuToggle.addEventListener("click", () => {
+    mobileMenu.classList.remove("hidden");
+});
+
+// Evento para fechar o menu
+closeMenu.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden");
+});
+
 window.addEventListener('scroll', function() {
     const header = document.getElementById('menu');
     const scrollPosition = window.scrollY;
